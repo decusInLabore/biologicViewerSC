a298df9746254a902f35800f6897b59f7db97cc5
decusinlabore <stefan.boeing@yahoo.com>
2021-09-07 07:49:54 +0100

    tracked splitOptions

--- inst/scAppTemplate/R/fct_helpers.R
+++ inst/scAppTemplate/R/fct_helpers.R
@@ -38,11 +38,7 @@ plot_prep_server <- function(
   maxExpr = NULL,
   showPlotLegend = FALSE
 ) {
-  #library(ggplot2)
-  
-  
-  
-  if (is.null(maxX)){
+   if (is.null(maxX)){
     maxX <- 1.1*max(df$x_axis, na.rm = T)  
   } 
   
@@ -58,34 +54,7 @@ plot_prep_server <- function(
     minY <- 1.1*min(df$y_axis, na.rm = T)  
   }
   
-  # ###########################################################################
-  # ## Determine split options                                               ##
-  # splitOptions <- names(df)
-  # 
-  # rmVec <- c(
-  #   grep("orig_", splitOptions),
-  #   grep("sampleID", splitOptions),
-  #   grep("old_ident", splitOptions),
-  #   grep("hmIdent", splitOptions),
-  #   grep("color", tolower(splitOptions)),
-  #   grep("lg10expr", tolower(splitOptions))
-  #   
-  # )
-  # 
-  # if (length(rmVec) > 0){
-  #   splitOptions <- splitOptions[-rmVec]
-  # }
-  # 
-  # 
-  # ## Remove all split options with more than 20 options ##
-  # Nopt <- apply(df[,splitOptions], 2, function(x) length(unique(x)))
-  # Nopt <- sort(Nopt[Nopt < 25], decreasing = F)
-  # 
-  # 
-  # splitOptions <- as.vector(names(Nopt))
-  # ## Done                                                                  ##
-  # ###########################################################################
-  # 
+  
   
   nCellsTotal <- nrow(df)
   nExpr <- nrow(df[df$gene != 0,])
@@ -94,8 +63,9 @@ plot_prep_server <- function(
   qGene <- qGene[qGene != 0]
   
   plotInput <- reactive({
-    
-    if (colorBy %in% splitOptions ){
+    startUpList <- golem::get_golem_options(which = "startUpList")
+    nonNumCols <- startUpList$utilityList$nonNumCols
+    if (colorBy %in% nonNumCols ){
       df$Dcolor[df$Dcolor == ""] <- "Rest"
       df$Dcolor <- factor(df$Dcolor)
     } else if( is.numeric( df$Dcolor ) ) {
@@ -156,8 +126,9 @@ plot_prep_server <- function(
     
     
     p <- p + ggplot2::xlab(x_axis) + ggplot2::ylab(y_axis)
-    
-    if (colorBy %in% splitOptions ){
+    startUpList <- golem::get_golem_options(which = "startUpList")
+    nonNumCols <- startUpList$utilityList$nonNumCols
+    if (colorBy %in% nonNumCols ){
         dfCol <- unique(df[,c(colorBy, "dotColor")])
         colVec <- dfCol$dotColor
         names(colVec) <- as.character(dfCol[,colorBy])
@@ -274,8 +245,6 @@ plot_prep_server_dl <- function(
   maxExpr = NULL,
   showPlotLegend = FALSE
 ) {
-  #library(ggplot2)
-  
   if (is.null(maxX)){
     maxX <- 1.1*max(df$x_axis, na.rm = T)  
   } 
@@ -292,33 +261,6 @@ plot_prep_server_dl <- function(
     minY <- 1.1*min(df$y_axis, na.rm = T)  
   }
   
-  ###########################################################################
-  ## Determine split options                                               ##
-  # splitOptions <- names(df)
-  # 
-  # rmVec <- c(
-  #   grep("orig_", splitOptions),
-  #   grep("sampleID", splitOptions),
-  #   grep("old_ident", splitOptions),
-  #   grep("hmIdent", splitOptions),
-  #   grep("color", tolower(splitOptions))
-  #   
-  # )
-  # 
-  # if (length(rmVec) > 0){
-  #   splitOptions <- splitOptions[-rmVec]
-  # }
-  # 
-  # 
-  # ## Remove all split options with more than 20 options ##
-  # Nopt <- apply(df[,splitOptions], 2, function(x) length(unique(x)))
-  # Nopt <- sort(Nopt[Nopt < 25], decreasing = F)
-  # 
-  # 
-  # splitOptions <- as.vector(names(Nopt))
-  # splitOptions <- splitOptions[splitOptions != "lg10Expr"]
-  ## Done                                                                  ##
-  ###########################################################################
   
   
   nCellsTotal <- nrow(df)
@@ -327,150 +269,153 @@ plot_prep_server_dl <- function(
   qGene <- unique(na.omit(df$gene))
   qGene <- qGene[qGene != 0]
   
-  #plotInput <- reactive({
-  
-  if (colorBy %in% splitOptions ){
-    df$Dcolor[df$Dcolor == ""] <- "Rest"
-    df$Dcolor <- factor(df$Dcolor)
-  } else if( is.numeric( df$Dcolor ) ) {
-    minExpr <- floor(min(df$Dcolor, na.rm = T))
+  plotInput <- reactive({
     
-    if (is.null(maxExpr)){
-      maxExpr <- ceiling(max(df$Dcolor, na.rm = T))   
-      if (maxExpr == 1){
-        ceiling_dec <- function(x, level=1) round(x + 5*10^(-level-1), level)
-        maxExpr <- ceiling_dec(max(df$Dcolor, na.rm = T),2)   
+    if (colorBy %in% nonNumCols ){
+      df$Dcolor[df$Dcolor == ""] <- "Rest"
+      df$Dcolor <- factor(df$Dcolor)
+    } else if( is.numeric( df$Dcolor ) ) {
+      minExpr <- floor(min(df$Dcolor, na.rm = T))
+      
+      if (is.null(maxExpr)){
+        maxExpr <- ceiling(max(df$Dcolor, na.rm = T))   
+        if (maxExpr == 1){
+          ceiling_dec <- function(x, level=1) round(x + 5*10^(-level-1), level)
+          maxExpr <- ceiling_dec(max(df$Dcolor, na.rm = T),2)   
+        }
       }
-    }
-    
-  } else {
-    df$Dcolor[df$Dcolor == ""] <- "Rest"
-    df$Dcolor <- factor(df$Dcolor)
-  }     
-  
-  ###########################################################################
-  ## Determine plot logic                                                  ##
-  if (is.numeric(df$x_axis)){
-    #######################################################################
-    ## Decide on factorial display logic                                 ##
-    if (df$y_axis[1] == "Densityplot"){
-      plotLogic <- "density"
-      p <- ggplot2::ggplot(
-        data = df, ggplot2::aes(x=x_axis, y=..density.., color=Dcolor,fill=Dcolor)
-      ) + ggplot2::geom_density(alpha=0.3, position="stack") 
-    } else if (df$y_axis[1] == "Histogram"){
-      plotLogic <- "histogram"
-      Nbin <- ceiling(length(df$x_axis)/5)
-      p <- ggplot2::ggplot(
-        data = df, ggplot2::aes(x=x_axis, color=Dcolor,fill=Dcolor)
-      ) + ggplot2::geom_histogram(alpha=0.3, position="stack", bins = Nbin) 
-    } else {
-      plotLogic <- "point"
-      p <- ggplot2::ggplot(
-        data = df, ggplot2::aes(x_axis, y_axis, color=Dcolor)
-      ) + ggplot2::geom_point(
-        shape = 16,
-        size = as.numeric(dotsize)
-      ) 
-    }
-    ## Done deciding factorial display logic
-    #########################################################################  
-  } else {
-    
-    plotLogic <- "violin"
-    p <- ggplot2::ggplot(
-      data = df, ggplot2::aes(x_axis, y_axis, color=Dcolor)
-    ) + ggplot2::geom_violin(trim=FALSE, fill="#E8E8E8"
-    )+ ggplot2::geom_jitter(height = 0, size = as.numeric(dotsize))
-  }
-  ## Done plot logic                                                       ##
-  ###########################################################################
-  
-  
-  p <- p + ggplot2::xlab(x_axis) + ggplot2::ylab(y_axis)
-  
-  if (colorBy %in% splitOptions ){
-    dfCol <- unique(df[,c(colorBy, "dotColor")])
-    colVec <- dfCol$dotColor
-    names(colVec) <- as.character(dfCol[,colorBy])
-    colVec <- colVec[colVec != ""]
-    
-    
-    p <- p + ggplot2::scale_colour_manual(colorBy ,values = colVec
-    ) + ggplot2::guides(col = guide_legend(override.aes = list(shape = 16, size = 5))
-    )
-    
-    if (plotLogic %in% c("density", "histogram")){
-      p <- p + ggplot2::scale_fill_manual(colorBy ,values = colVec
-      ) + ggplot2::guides(col = guide_legend(override.aes = list(shape = 16, size = 5))
-      )
-    }
-    
-  } else if (is.numeric( df$Dcolor )){
-    if (minExpr < 0){
-      p <- p + ggplot2::scale_color_gradient2("Expr",low= lowColor, mid = "white", high= dotcolor, midpoint = 0, limits=c(minExpr,maxExpr)
-      )
       
     } else {
-      p <- p + ggplot2::scale_color_gradient("Expr",low= lowColor, high= dotcolor, limits=c(minExpr,maxExpr)
+      df$Dcolor[df$Dcolor == ""] <- "Rest"
+      df$Dcolor <- factor(df$Dcolor)
+    }     
+    
+    
+    
+    ###########################################################################
+    ## Determine plot logic                                                  ##
+    if (is.numeric(df$x_axis)){
+      #######################################################################
+      ## Decide on factorial display logic                                 ##
+      if (df$y_axis[1] == "Densityplot"){
+        plotLogic <- "density"
+        p <- ggplot2::ggplot(
+          data = df, ggplot2::aes(x=x_axis, y=..density.., color=Dcolor,fill=Dcolor)
+        ) + ggplot2::geom_density(alpha=0.3, position="stack") 
+      } else if (df$y_axis[1] == "Histogram"){
+        plotLogic <- "histogram"
+        Nbin <- ceiling(length(df$x_axis)/5)
+        p <- ggplot2::ggplot(
+          data = df, ggplot2::aes(x=x_axis, color=Dcolor,fill=Dcolor)
+        ) + ggplot2::geom_histogram(alpha=0.3, position="stack", bins = Nbin) 
+      } else {
+        plotLogic <- "point"
+        p <- ggplot2::ggplot(
+          data = df, ggplot2::aes(x_axis, y_axis, color=Dcolor)
+        ) + ggplot2::geom_point(
+          shape = 16,
+          size = as.numeric(dotsize)
+        ) 
+      }
+      ## Done deciding factorial display logic
+      #########################################################################  
+    } else {
+      
+      plotLogic <- "violin"
+      p <- ggplot2::ggplot(
+        data = df, ggplot2::aes(x_axis, y_axis, color=Dcolor)
+      ) + ggplot2::geom_violin(trim=FALSE, fill="#E8E8E8"
+      )+ ggplot2::geom_jitter(height = 0, size = as.numeric(dotsize))
+    }
+    ## Done plot logic                                                       ##
+    ###########################################################################
+    
+    
+    p <- p + ggplot2::xlab(x_axis) + ggplot2::ylab(y_axis)
+    
+    if (colorBy %in% nonNumCols ){
+      dfCol <- unique(df[,c(colorBy, "dotColor")])
+      colVec <- dfCol$dotColor
+      names(colVec) <- as.character(dfCol[,colorBy])
+      colVec <- colVec[colVec != ""]
+      
+      
+      p <- p + ggplot2::scale_colour_manual(colorBy ,values = colVec
+      ) + ggplot2::guides(col = guide_legend(override.aes = list(shape = 16, size = 5))
+      )
+      
+      if (plotLogic %in% c("density", "histogram")){
+        p <- p + ggplot2::scale_fill_manual(colorBy ,values = colVec
+        ) + ggplot2::guides(col = guide_legend(override.aes = list(shape = 16, size = 5))
+        )
+      }
+      
+    } else if (is.numeric( df$Dcolor )){
+      if (minExpr < 0){
+        p <- p + ggplot2::scale_color_gradient2("Expr",low= lowColor, mid = "white", high= dotcolor, midpoint = 0, limits=c(minExpr,maxExpr)
+        )
+        
+      } else {
+        p <- p + ggplot2::scale_color_gradient("Expr",low= lowColor, high= dotcolor, limits=c(minExpr,maxExpr)
+        )
+      }
+      
+    } 
+    
+    
+    if (background == "white"){
+      p <- p + ggplot2::theme_bw()
+    } else if (background == "minimal"){
+      p <- p + ggplot2::theme_minimal()
+    } else if (background == "plain"){
+      p <- p + ggplot2::theme_void()
+    } else {
+      p <- p + ggplot2::theme(
+        panel.background = ggplot2::element_rect(fill = "lightgrey")
       )
     }
     
-  } 
-  
-  
-  if (background == "white"){
-    p <- p + ggplot2::theme_bw()
-  } else if (background == "minimal"){
-    p <- p + ggplot2::theme_minimal()
-  } else if (background == "plain"){
-    p <- p + ggplot2::theme_void()
-  } else {
     p <- p + ggplot2::theme(
-      panel.background = ggplot2::element_rect(fill = "lightgrey")
-    )
-  }
-  
-  p <- p + ggplot2::theme(
-    axis.text.y   = ggplot2::element_text(size=8),
-    axis.text.x   = ggplot2::element_text(size=8),
-    axis.title.y  = ggplot2::element_text(size=8),
-    axis.title.x  = ggplot2::element_text(size=8),
-    axis.line = ggplot2::element_line(colour = "black"),
-    panel.border = ggplot2::element_rect(colour = "black", fill=NA, size=1),
-    plot.title = ggplot2::element_text(hjust = 0.5, size = 12)
-  ) 
-  
-  if (is.numeric(df$x_axis)){
-    p <- p + ggplot2::xlim(minX, maxX) 
-  }
-  
-  if (is.numeric(df$y_axis)){
-    p <- p + ggplot2::ylim(minY, maxY) 
-  }
-  
-  if (colorBy == "lg10Expr" | x_axis == "lg10Expr" | y_axis == "lg10Expr") {
-    titleString <- paste0("Sample: ", plot_name, " ", nExpr, "/", nCellsTotal, " cells (",percExpr,"%) express ", geneSel)
-  } else {
-    titleString <-paste0("Sample: ", plot_name)
-  }
-  
-  p <- p + ggplot2::ggtitle(titleString) 
-  #+ ggtitle(paste0("Gene ", input$gene, " in sample ", conditionVec[i], " (E:",cellsExpressingGene[i],"/NE:",cellsNotExpressingGene[i], ", ",percE[i],"%)")) + scale_size_continuous(limits = c(0, maxExpr)
-  #) #+ xlim(minX, maxX) + ylim(minY, maxY)
-  
-  posX <- grep("UMAP", x_axis)
-  posY <- grep("UMAP", y_axis)
-  if ( (length(posX) == 1) & (length(posY) == 1)){
-    p <-  p + ggplot2::coord_fixed()
-  }
-  
-  if (!showPlotLegend){
-    p <- p + theme(legend.position = "none")
-  } 
-  
-  
-  p
+      axis.text.y   = ggplot2::element_text(size=8),
+      axis.text.x   = ggplot2::element_text(size=8),
+      axis.title.y  = ggplot2::element_text(size=8),
+      axis.title.x  = ggplot2::element_text(size=8),
+      axis.line = ggplot2::element_line(colour = "black"),
+      panel.border = ggplot2::element_rect(colour = "black", fill=NA, size=1),
+      plot.title = ggplot2::element_text(hjust = 0.5, size = 12)
+    ) 
+    
+    if (is.numeric(df$x_axis)){
+      p <- p + ggplot2::xlim(minX, maxX) 
+    }
+    
+    if (is.numeric(df$y_axis)){
+      p <- p + ggplot2::ylim(minY, maxY) 
+    }
+    
+    if (colorBy == "lg10Expr" | x_axis == "lg10Expr" | y_axis == "lg10Expr") {
+      titleString <- paste0("Sample: ", plot_name, " ", nExpr, "/", nCellsTotal, " cells (",percExpr,"%) express ", geneSel)
+    } else {
+      titleString <-paste0("Sample: ", plot_name)
+    }
+    
+    p <- p + ggplot2::ggtitle(titleString) 
+    #+ ggtitle(paste0("Gene ", input$gene, " in sample ", conditionVec[i], " (E:",cellsExpressingGene[i],"/NE:",cellsNotExpressingGene[i], ", ",percE[i],"%)")) + scale_size_continuous(limits = c(0, maxExpr)
+    #) #+ xlim(minX, maxX) + ylim(minY, maxY)
+    
+    posX <- grep("UMAP", x_axis)
+    posY <- grep("UMAP", y_axis)
+    if ( (length(posX) == 1) & (length(posY) == 1)){
+      p <-  p + ggplot2::coord_fixed()
+    }
+    
+    if (!showPlotLegend){
+      p <- p + theme(legend.position = "none")
+    } 
+    
+    
+    p
+  })
   # })
   ## End of download functions   
 }
