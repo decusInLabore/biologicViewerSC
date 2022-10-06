1fd33ff22d3a83e0a13b6652f43eca61769d455e
Stefan Boeing <boeings@babs000.camp.thecrick.org>
2022-10-06 10:04:16 +0100

    Updated app

--- inst/scAppTemplate/app.R
+++ inst/scAppTemplate/app.R
@@ -3,6 +3,7 @@
 # Or use the blue button on top of this file
 
 
+
 pkgload::load_all(export_all = FALSE,helpers = FALSE,attach_testthat = FALSE)
 options( "golem.app.prod" = TRUE)
 biologicViewerSC::run_app() # add parameters here (if any)
